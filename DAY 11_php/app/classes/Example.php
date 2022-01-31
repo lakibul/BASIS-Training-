90814ceefe852b94cf22e3c61adbf8d7ff0ec670
lakibul <lakibul.cse@gmail.com>
2022-01-31 13:13:00 +0600

    cmplt

--- DAY 11_php/app/classes/Example.php
+++ DAY 11_php/app/classes/Example.php
@@ -6,11 +6,82 @@ class Example
 
     public function index()
     {
-        $this->student = [50, 20, 'BITM', 10.20, true, false];
-//        echo $this->student;
-        foreach($this->student as $item)
-        {
-            echo $item.' ';
-        }
+        $this->student = [
+            0 => [
+                'name'=> 'Lakib',
+                'email'=>"lakibul.cse@gmail.com",
+                'mobile'=>'017886355566'
+            ],
+            1 => [
+                'name'=> 'Habib',
+                'email'=>"habib.cse@gmail.com",
+                'mobile'=>[
+                    'personal' => '015866645634',
+                    'Other' => '01245799345',
+                ],
+            ],
+            2 => 'BITM',
+            3 => [
+                'name'=> 'Munna',
+                'email'=>"munna.cse@gmail.com",
+                'mobile'=> [
+                  'Personal' => '0154335656',
+                  'Other' => '0145335654',
+
+                ],
+            ],
+        ];
+
+
+        echo '<pre>';
+        print_r($this->student);
+
+
+//        foreach ($this->student as $item)
+//        {
+//            if(is_array($item))
+//            {
+//                foreach ($item as $value)
+//                {
+//                    if (is_array($value))
+//                    {
+//                        foreach ($value as $v_value)
+//                        {
+//                            echo $v_value.' ';
+//                        }
+//                    }
+//                    else
+//                    {
+//                        echo $value.' ';
+//                    }
+//                }
+//            }
+//            else
+//            {
+//                echo $item;
+//            }
+//            echo '<br/>';
+//        }
+
+
+
+//        foreach ($this->student as $key => $item)
+//        {
+//            if($key == 0)
+//            {
+//                echo $item['name'].' '. $item['email'].' '.$item['mobile'],'<br/>';
+//                break;
+//            }
+//
+//        }
+
+//        echo $this->student[1]['email'];
+
+//        $this->student = [50, 20, 'BITM', 10.20, true, false];
+////        echo $this->student;
+//        foreach($this->student as $key => $student)
+//        {
+//            echo 'index no - '. $key. ' value - '.$student.'<br/>';
+//        }
     }
 }
